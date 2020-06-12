917c11479f898cf6e3a3c9f474b038febfa6040e
Daman Mohan Kumar <kumardm@uci.edu>
2020-06-12 02:31:04 -0700

    Add header guard.

--- arch/x86_64/types.h
+++ arch/x86_64/types.h
@@ -1,3 +1,5 @@
+#ifndef __XV6_TYPES_H_
+#define __XV6_TYPES_H_
 // Use fixed width integers from stdint.h, and more importantly,
 // uintptr_t which has the correct width depending on whether
 // we are compiling 32- or 64-bit code.
@@ -7,6 +9,7 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 
+typedef uint8_t  uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
@@ -14,3 +17,4 @@ typedef uint64_t uint64;
 typedef uintptr_t uintp;
 
 typedef uintp pde_t;
+#endif // __XV6_TYPES_H_
