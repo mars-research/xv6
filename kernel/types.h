d3baca67c5117d336eddf2f8d40178643df0b681
Daman Mohan Kumar <kumardm@uci.edu>
2020-06-14 14:40:32 -0700

    Merge types.h

--- kernel/types.h
+++ kernel/types.h
@@ -1,10 +1,20 @@
+#ifndef __XV6_TYPES_H_
+#define __XV6_TYPES_H_
+// Use fixed width integers from stdint.h, and more importantly,
+// uintptr_t which has the correct width depending on whether
+// we are compiling 32- or 64-bit code.
+#include "stdint.h"
+
 typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int  uint32;
-typedef unsigned long uint64;
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
-typedef uint64 pde_t;
+typedef uintptr_t uintp;
+
+typedef uintp pde_t;
+#endif // __XV6_TYPES_H_
