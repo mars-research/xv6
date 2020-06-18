82c28dfc9c80af0638fc6657d6c92ed8d970b883
Daman Mohan Kumar <kumardm@uci.edu>
2020-06-17 23:52:46 -0700

    Update proc struct

--- kernel/arch/x86_64/proc.h
+++ kernel/arch/x86_64/proc.h
@@ -32,24 +32,30 @@ struct context {
   uint64 r12;
   uint64 r11;
   uint64 rbx;
-  uint64 ebp; //rbp
-  uint64 eip; //rip;
+  uint64 rbp;
+  uint64 rip;
 };
 
 enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
 struct proc {
-  char *kstack;                // Bottom of kernel stack for this process, must be first entry
-  uint64 sz;                   // Size of process memory (bytes)
-  pde_t* pgdir;                // Page table
+  struct spinlock lock;
+
+  // p->lock must be held whenusing these:
   enum procstate state;        // Process state
-  int pid;                     // Process ID
   struct proc *parent;         // Parent process
-  struct trapframe *tf;        // Trap frame for current syscall
-  struct context *context;     // swtch() here to run process
   void *chan;                  // If non-zero, sleeping on chan
   int killed;                  // If non-zero, have been killed
+  int pid;                     // Process ID
+  int xstate;                  // Exit status to be returned to parent's wait
+
+  // these are private to the process, so lock need not be held
+  uint64 kstack;               // Bottom of kernel stack for this process, must be first entry
+  uint64 sz;                   // Size of process memory (bytes)
+  pml4e_t* pml4;               // Page table
+  struct trapframe *tf;        // Trap frame for current syscall
+  struct context *context;     // swtch() here to run process
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
