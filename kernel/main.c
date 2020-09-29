714a9efd25ec0e2179c67a2e8d3d3a1f382dd274
Daman Mohan Kumar <kumardm@uci.edu>
2020-09-29 16:40:15 -0700

    added calls to console IO initialization, first user process initialization, and scheduler
    - only bootstrap processor does any work

--- kernel/main.c
+++ kernel/main.c
@@ -1,36 +1,33 @@
 #include "types.h"
-#include "param.h"
-#include "memlayout.h"
 #include "defs.h"
 
 volatile static int started = 0;
-extern uint64 end[];
 
 // start() jumps here in supervisor mode on all CPUs.
 // TODO: remove all references to x86-specific functions here
 void
 main()
 {
-  if (0 == cpuid()) {
-    consoleinit();
-    printfinit();
-    printf("\n");
-    printf("xv6 is booting\n");
-    printf("\n");
-    kinit();
-    kpaginginit();
-    procinit();
-    trapinit();
-    seginit();
-    lapicinit();
-    picinit();
-    ioapicinit();
-    binit();
-    iinit();
-    fileinit();
-    diskinit();
-    userinit();
-    printf("done\n");
-    while (1);
-  }
+  kinit();
+  kpaginginit();
+  procinit();
+  trapinit();
+  seginit();
+  mpinit();
+  lapicinit();
+  picinit();
+  ioapicinit();
+  consoleinit();
+  printfinit();
+  printf("\n");
+  printf("xv6 is booting\n");
+  printf("\n");
+  binit();
+  iinit();
+  fileinit();
+  diskinit();
+  userinit();
+  printf("done\n");
+
+  scheduler();
 }
